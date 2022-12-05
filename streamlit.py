f7b69e41623310414c164cfc2fd8a4b54861fed4
Bram Augenbroe <r0841633@student.thomasmore.be>
2022-12-05 13:56:33 +0100

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+#header = st.container()
+#dataset = st.container()
+#features = st.container()
+#modelTraining = st.container()
+
+with header:
+    st.Title('Welcome to our Big Data project!')

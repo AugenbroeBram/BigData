f599acc012732794636e2be2aaf9d20d6289f13b
Bram Augenbroe <r0841633@student.thomasmore.be>
2022-12-05 14:17:22 +0100

    jonge

--- streamlit.py
+++ streamlit.py
@@ -1,9 +1,3 @@
 import streamlit as st
 
-#header = st.container()
-#dataset = st.container()
-#features = st.container()
-#modelTraining = st.container()
-
-with header:
-    st.Title('Welcome to our Big Data project!')
+st.write("hello ** world **")
